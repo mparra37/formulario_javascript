f6bffa95541012300712059d0d462be40df9a85c
Mario Parra <marioparra375@gmail.com>
2023-11-06 19:27:27 -0800

    Add delete and edit buttons

--- script.js
+++ script.js
@@ -1,32 +1,35 @@
 document.getElementById("formulario").addEventListener('submit', function(e){
-
+	//Previene que se refresque la página
 	e.preventDefault();
 
-	var estudiante = {
-		nombre: document.getElementById("nombre").value,
-		apellido: document.getElementById("apellido").value,
-		correo: document.getElementById("correo").value,
-		edad: document.getElementById("edad").value,
-		hobbies: document.getElementById("hobbies").value
+	var pelicula = {
+		titulo: document.getElementById("titulo").value,
+		anio: document.getElementById("anio").value,
+		duracion: document.getElementById("duracion").value,
+		genero: document.getElementById("genero").value,
+		director: document.getElementById("director").value,
+		sinopsis: document.getElementById("sinopsis").value
 	};
 
-	agregarTabla(estudiante);
+	agregarTabla(pelicula);
 
+	//Pone los datos en blanco
 	e.target.reset();
 });
 
-function agregarTabla(estudiante){
+function agregarTabla(pelicula){
 
 	var tbody = document.getElementById("cuerpoTabla");
 
 	var fila = document.createElement("tr");
 
-	for(var key in estudiante){
+	for(var key in pelicula){
 		var td = document.createElement("td");
-		td.textContent = estudiante[key];
+		td.textContent = pelicula[key];
 		fila.appendChild(td);
 	}
 
+	
 	var td = document.createElement("td");
 	var boton = document.createElement("button");
 	boton.textContent = "Eliminar";
@@ -36,6 +39,23 @@ function agregarTabla(estudiante){
 	}
 	td.appendChild(boton);
 	fila.appendChild(td);
+
+
+	var td2 = document.createElement("td");
+	var boton2 = document.createElement("button");
+	boton2.textContent = "Editar";
+	boton2.classList.add("btn", "btn-success");
+	boton2.onclick = function(){
+			document.getElementById("titulo").value = fila.children[0].innerHTML;
+			document.getElementById("anio").value = fila.children[1].innerHTML;
+			document.getElementById("duracion").value = fila.children[2].innerHTML;	
+			document.getElementById("genero").value = fila.children[3].innerHTML;		
+			document.getElementById("director").value = fila.children[4].innerHTML;
+			document.getElementById("sinopsis").value = fila.children[5].textContent;
+
+	};
+	td2.appendChild(boton2);
+	fila.appendChild(td2);
 
 
 
